ee7fe41c051d65b54822c2717f621cf9f9638aaf
luck-fc <xiaoorchao@gmail.com>
2017-03-27 19:01:35 +0800

    Added ConstrainLayout2017_3_21

---
+++ android/ui/ConstraintLayout_2017_3_21/app/src/main/java/top/goluck/constraintlayout_2017_3_21/MainActivity.java
@@ -0,0 +1,16 @@
+package top.goluck.constraintlayout_2017_3_21;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main1);
+//        setContentView(R.layout.activity_main2);
+//        setContentView(R.layout.activity_main3);
+    }
+}
