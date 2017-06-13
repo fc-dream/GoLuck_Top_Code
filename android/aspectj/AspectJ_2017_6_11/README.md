f3eb902c6e031c76dd5bdc7dde8a047eb1a56bed
luck-fc <xiaoorchao@gmail.com>
2017-06-13 19:43:00 +0800

    Added AspectJ_2017_6_11

---
+++ android/aspectj/AspectJ_2017_6_11/README.md
@@ -0,0 +1,3 @@
+# AspectJ_2017_6_11
+* gradlew assemble 生成所有apk
+* gradlew assemble + 渠道 生成指定渠道apk
--- android/aspectj/AspectJ_2017_6_11/app/src/main/java/top/goluck/aspectj_2017_6_11/MainActivity.java
+++ android/aspectj/AspectJ_2017_6_11/app/src/main/java/top/goluck/aspectj_2017_6_11/MainActivity.java
@@ -1,5 +1,7 @@
 package top.goluck.aspectj_2017_6_11;
 
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -11,6 +13,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         LogUtil.i(false,"onCreate 我的channel值 = "+BuildConfig.channel);
         onTest("我是第一个满足条件的参数",1,true);
+        ApplicationInfo appInfo = null;
+        try {
+            appInfo = getPackageManager().getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        String UMENG_CHANNEL = appInfo.metaData.getString("UMENG_CHANNEL");
+        LogUtil.i(false,"读取AndroidManifest.xml的UMENG_CHANNEL="+UMENG_CHANNEL);
     }
 
     public String onTest(String arg0,int arg1,boolean arg2){
